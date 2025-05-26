a303e2b17cabdb96631d7987e2044815201538a3
Binh Vuong Le Duc <ducbinhhn2002@gmail.com>
2025-05-26 22:09:18 +0200

    feat: setup vite, lint, codebase

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/config';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    globals: true,
+    exclude: [...configDefaults.exclude, '**/e2e/**'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+});
